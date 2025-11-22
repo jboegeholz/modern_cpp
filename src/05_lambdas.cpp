b3ae68ae0aa3ed06ed590475c872a0cd74677566
jboegeholz <boegeholz.joern@gmail.com>
2025-11-22 10:07:52 +0100

    add lambdas

--- src/05_lambdas.cpp
+++ src/05_lambdas.cpp
@@ -12,21 +12,25 @@ int main() {
     }
     std::cout << std::endl;
 
-    std::sort(numbers.begin(), numbers.end());
-    for (float x : numbers) {
+    std::ranges::sort(numbers);
+    for (const float x : numbers) {
         std::cout << x << " < ";
     }
     std::cout << std::endl;
 
-    std::sort(numbers.begin(), numbers.end(),
-            [](float a, float b) {
-                return std::abs(a) < std::abs(b);
-            }
-        );
+    std::ranges::sort(numbers,
+                      [](float a, float b) {
+                          return std::abs(a) < std::abs(b);
+                      }
+    );
+    for (const float x : numbers) {
+        std::cout << x << " ";
+    }
+    std::cout << std::endl;
 
-    std::ranges::sort(v, {}, [](float x) { return std::abs(x); });
+    std::ranges::sort(numbers, {}, [](float x) { return std::abs(x); });
 
-    for (float x : numbers) {
+    for (const float x : numbers) {
         std::cout << x << " ";
     }
     std::cout << std::endl;
