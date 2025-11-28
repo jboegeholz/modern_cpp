a1b7768a227cbf873906f363fb0bc1c5ae7f9357
jboegeholz <boegeholz.joern@gmail.com>
2025-11-28 17:17:54 +0100

    fix

--- src/07_ranges.cpp
+++ src/07_ranges.cpp
@@ -8,7 +8,7 @@ int main() {
     const std::vector<float> sorted = {-11.0, -3.5, -2.0, 1.0};
     const std::vector<float> sorted_desc = { 1.0, -2.0, -3.5, -11.0};
 
-    std::ranges::sort(numbers.begin(), numbers.end());
+    std::ranges::sort(numbers);
     assert (numbers == sorted);
 
     std::ranges::sort(numbers, {}, [](const float x) { return std::abs(x); });
