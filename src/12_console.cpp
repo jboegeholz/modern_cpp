3289a5d3406db99e85fe803e3bc2a3c443147822
jboegeholz <boegeholz.joern@gmail.com>
2025-11-29 18:02:13 +0100

    rework

--- src/12_console.cpp
+++ src/12_console.cpp
@@ -1,7 +1,6 @@
 #include <iostream>
 
-int main()
-{
+void time_endl() {
     auto start_1 = std::chrono::high_resolution_clock::now();
     for (int i = 0; i < 1'000'000; ++i) {
         std::cout << i << '\n';
@@ -20,5 +19,10 @@ int main()
     std::cout << "time for CR " << duration.count() << " microseconds\n";
     duration = std::chrono::duration_cast<std::chrono::microseconds>(end_2 - start_2);
     std::cout << "time for std::endl " << duration.count() << " microseconds\n";
+}
+
+int main() {
+    time_endl();
+    std::cout << "Hello modern C++" << '\n';
 }
 
