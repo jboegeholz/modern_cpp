dc98167a85ff36310eb3f5d5180fc2dd9342ff26
jboegeholz <boegeholz.joern@gmail.com>
2025-12-05 10:10:39 +0100

    refactor vector

--- src/04_vectors.cpp
+++ src/04_vectors.cpp
@@ -6,36 +6,53 @@
 
 int main()
 {
-    std::vector<int> coll;
-    assert( coll.capacity() == 0);
-    assert( coll.empty());
+    std::vector<int> int_vector;
+    std::vector<double> double_vector;
+    assert( int_vector.capacity() == 0);
+    assert( int_vector.empty());
 
     // add values at back
-    coll.push_back(1);
-    coll.push_back(2);
-    coll.push_back(3);
-    assert( coll.size() == 3);
+    int_vector.push_back(1);
+    int_vector.push_back(2);
+    int_vector.push_back(3);
+    assert( int_vector.size() == 3);
+    assert( int_vector.capacity() == 3);
+    int_vector.push_back(4);
+    assert( int_vector.size() == 4);
+    assert( int_vector.capacity() == 4);
+    int_vector.push_back(5);
+    assert( int_vector.size() == 5 );
+    assert( int_vector.capacity() == 6);
 
     // get element via index
-    assert(coll.at(1) == 2);
+    assert(int_vector.at(1) == 2);
 
     // out of range
     try {
-        coll.at(3) = 4;      // vector::at throws an out-of-range
+        int_vector.at(10) = 4;      // vector::at throws an out-of-range
     }
     catch (const std::out_of_range& oor) {
         std::cerr << "Out of Range error: " << oor.what() << '\n';
     }
 
     // get last element
-    assert(coll.back() == 3);
+    assert(int_vector.back() == 5);
 
     // get first element
-    assert(coll.front() == 1);
+    assert(int_vector.front() == 1);
 
     // remove last element
-    coll.pop_back();
-    assert(coll.size() == 2);
+    int_vector.pop_back();
+    assert(int_vector.size() == 4);
 
+    std::vector<int> v;
+    for (int i = 0; i < 20; ++i) {
+        v.push_back(i);
+        std::cout << "Size: " << v.size() << ", Capacity: " << v.capacity() << '\n';
+    }
+
+
+    std::vector<std::string> str_vector;
+    str_vector.emplace_back("Hello");
 
 }
