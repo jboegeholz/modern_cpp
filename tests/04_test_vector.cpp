847248f6e4d8109f471fab5d6e220acbe440859e
jboegeholz <boegeholz.joern@gmail.com>
2025-12-22 10:32:17 +0100

    add google test

--- tests/04_test_vector.cpp
+++ tests/04_test_vector.cpp
@@ -1,6 +1,23 @@
 #include <gtest/gtest.h>
 #include <vector>
 
+struct Vec3D
+{
+    int x, y, z;
+    Vec3D(const int x, const int y, const int z) : x(x), y(y), z(z) {}
+    bool operator==(const Vec3D & rhs) const
+    {
+        if (x == rhs.x && y == rhs.y && z == rhs.z)
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+};
+
 TEST(VectorTest, VectorInitialSize) {
     const std::vector<int> int_vector;
     EXPECT_EQ(int_vector.capacity(), 0);
@@ -46,4 +63,27 @@ TEST(VectorTest, VectorGetElement) {
 TEST(VectorTest, VectorGetElementException) {
     const std::vector<int> int_vector = {1,2,3,4,5};
     EXPECT_THROW((void)int_vector.at(10), std::out_of_range);
+}
+
+TEST(VectorTest, VectorElementBackFront) {
+    const std::vector<int> int_vector = {1,2,3};
+    EXPECT_EQ(int_vector.front(), 1);
+    EXPECT_EQ(int_vector.back(), 3);
+}
+
+TEST(VectorTest, VectorElementPopBack) {
+    std::vector<int> int_vector = {1,2,3};
+    int_vector.pop_back();
+    EXPECT_EQ(int_vector.size(), 2);
+}
+TEST(VectorTest, VectorElementInsert) {
+    std::vector<int> int_vector = {1,2,3};
+    int_vector.insert(int_vector.begin(), 0);
+    EXPECT_EQ(int_vector.at(0), 0);
+}
+
+TEST(VectorTest, VectorEmplaceBack) {
+    std::vector<Vec3D> my_vector;
+    my_vector.emplace_back(1, 2, 3 );
+    EXPECT_EQ(my_vector.at(0), Vec3D(1, 2, 3));
 }
