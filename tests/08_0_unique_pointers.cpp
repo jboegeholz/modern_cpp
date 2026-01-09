eca4b8a3cdb87023495703791e327d015c9265f4
jboegeholz <boegeholz.joern@gmail.com>
2026-01-09 15:06:45 +0100

    rework to tests

--- tests/08_0_unique_pointers.cpp
+++ tests/08_0_unique_pointers.cpp
@@ -1,14 +1,42 @@
 #include <gtest/gtest.h>
-#include <cstddef>
 #include <memory>
 
+struct Vec3D
+{
+    int x{0};
+    int y{0};
+    int z{0};
+};
 
-void double_unique_ptr() {
-    auto * p = new int(10);
-    const std::unique_ptr<int> ptr1 {p};
-    const std::unique_ptr<int> ptr2 {p};
+TEST(MyUniquePointer, UniquePointer)
+{
+    std::unique_ptr<Vec3D> my_pointer(new Vec3D);
+    my_pointer->x = 42;
+    EXPECT_EQ(my_pointer->x, 42);
 }
-TEST(MyUniquePointer, DoubleUniquePointer) {
+
+TEST(MyUniquePointer, MakeUniquePointer)
+{
+    auto my_pointer = std::make_unique<Vec3D>();
+    my_pointer->x = 42;
+    EXPECT_EQ(my_pointer->x, 42);
+}
+
+void double_unique_ptr()
+{
+    auto* p = new int(10);
+    const std::unique_ptr<int> ptr1{p};
+    const std::unique_ptr<int> ptr2{p};
+}
+
+TEST(MyUniquePointer, DoubleUniquePointer)
+{
     EXPECT_DEATH(double_unique_ptr(), ".*");
 }
-
+TEST(MyUniquePointer, Release)
+{
+    auto p = std::make_unique<int>(10);
+    const int* raw = p.release();
+    EXPECT_EQ(*raw, 10);
+    EXPECT_EQ(p, nullptr);
+}
