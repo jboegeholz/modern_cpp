1ac09769f729a445ee016ea43dfe9dd282c6024c
jboegeholz <boegeholz.joern@gmail.com>
2026-01-11 20:25:52 +0100

    rework

--- tests/08_2_shared_pointer.cpp
+++ tests/08_2_shared_pointer.cpp
@@ -1,3 +1,16 @@
-//
-// Created by Jörn Bögeholz on 11.01.26.
-//
+#include <gtest/gtest.h>
+#include <memory>
+
+struct Vec3D
+{
+    int x{0};
+    int y{0};
+    int z{0};
+};
+
+TEST(MyUniquePointer, UniquePointer)
+{
+    const auto my_pointer = std::make_shared<Vec3D>();
+    my_pointer->x = 42;
+    EXPECT_EQ(my_pointer->x, 42);
+}
