e55938ed4b2c06791264428f8e888ec6e297e521
jboegeholz <boegeholz.joern@gmail.com>
2026-01-19 10:38:50 +0100

    rework to tests

--- tests/08_0_unique_pointers.cpp
+++ tests/08_0_unique_pointers.cpp
@@ -8,6 +8,26 @@ struct Vec3D
     int z{0};
 };
 
+TEST(MyUniquePointer, OldSchoolPointer)
+{
+    auto* my_pointer = new Vec3D;
+    my_pointer->x = 42;
+    EXPECT_EQ(my_pointer->x, 42);
+    delete my_pointer;
+    my_pointer = nullptr;
+    EXPECT_EQ(my_pointer, nullptr);
+}
+
+TEST(MyUniquePointer, OldSchoolPointerDoubleDelete)
+{
+    auto* my_pointer = new Vec3D;
+    my_pointer->x = 42;
+    EXPECT_EQ(my_pointer->x, 42);
+    delete my_pointer;
+    delete my_pointer;
+    EXPECT_EQ(my_pointer, nullptr);
+}
+
 TEST(MyUniquePointer, UniquePointer)
 {
     std::unique_ptr<Vec3D> my_pointer(new Vec3D);
