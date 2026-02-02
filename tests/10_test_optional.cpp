e082bb7a54d3ed7535b128acc19c97fbd5207873
jboegeholz <boegeholz.joern@gmail.com>
2026-02-02 16:34:28 +0100

    add expected

--- tests/10_test_optional.cpp
+++ tests/10_test_optional.cpp
@@ -1,3 +1,4 @@
+#include <expected>
 #include <iostream>
 #include <string>
 #include <optional>
@@ -26,4 +27,19 @@ TEST(OptionalTest, StdPrintExtrapolation) {
 
     const std::string output = testing::internal::GetCapturedStdout();
     EXPECT_EQ(output, "convert '42' to int: 42\nconvert ' 077' to int: 77\ncan't convert 'hello' to int\nconvert '0x33' to int: 0\n");
+}
+
+std::expected<int, std::string> divide(const int dividend, const int divisor)
+{
+    if (divisor == 0) {
+        return std::unexpected("Division by zero is not allowed.");
+    }
+    return dividend / divisor;
+}
+TEST(OptionalTest, ExpectedTest)
+{
+    const auto result = divide(10, 2);
+    EXPECT_EQ(5, result.value());
+    const auto result2 = divide(10, 0);
+    EXPECT_EQ(result2, std::unexpected("Division by zero is not allowed."));
 }
